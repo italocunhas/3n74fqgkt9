b3b603be3c8c055bd9b81e0c7688eda241240bf0
italocunhas <italocunhasilva1@gmail.com>
2024-08-24 03:14:05 -0300

    mudanca

--- controle.py
+++ controle.py
@@ -44,6 +44,48 @@ def relatorio():
     cursor = conexao.cursor()
     comando_SQL = 'select * from clientes'
     cursor.execute(comando_SQL)
+    leitura_clientes = cursor.fetchall()
+    
+    relatorio.tableClientes.setRowCount(len(leitura_clientes))
+    relatorio.tableClientes.setColumnCount(6)
+    
+    for i in range (0, len(leitura_clientes)):
+        for j in range(0, 6):
+            relatorio.tableClientes.setItem(i, j, QtWidgets.QTableWidgetItem(str(leitura_clientes[i][j])))
+            
+numero_id_geral = 0 
+def editar_dados():
+    global numero_id_geral
+    dados = relatorio.tableClientes.currentRow()
+    cursor = conexao.cursor()
+    cursor.execute('select id from clientes')
+    leitura_clientes = cursor.fetchall()
+    id_ativo = leitura_clientes [dados] [0]
+    cursor.execute('select * from clientes where id='+str(id_ativo))
+    leitura_clientes = cursor.fetchall()
+        
+    editar.show()
+    numero_id_geral = id_ativo
+    
+    editar.txtAlterarId.setText(str(leitura_clientes[0][0]))
+    editar.txtAlterarNome.setText(str(leitura_clientes[0][1]))
+    editar.txtAlterarCPF.setText(str(leitura_clientes[0][2]))
+    editar.txtAlterarIdade.setText(str(leitura_clientes[0][3]))
+    editar.txtAlterarRenda.setText(str(leitura_clientes[0][4]))
+    editar.txtAlterarSituacao.setText(str(leitura_clientes[0][5]))
+    
+def alteracao_de_dados():
+    global numero_id_geral
+    
+    id = editar.txtAlterarId.text()
+    nome = editar.txtAlterarNome.text()
+    cpf = editar.txtAlterarCPF.text()
+    idade = editar.txtAlterarIdade.text()
+    renda = editar.txtAlterarRenda.text()
+    situacao = editar.txtAlterarSituacao.text()
+    
+    cursor = conexao.cursor()
+    
     
 app=QtWidgets.QApplication([])
 cadastros=uic.loadUi('cadastros.ui')
@@ -52,8 +94,9 @@ cadastros.btnAnalise.clicked.connect(analise)
 cadastros.btnRelatorio.clicked.connect(relatorio)
 
 relatorio=uic.loadUi('relatorio.ui')
+relatorio.btnEditar.clicked.connect(editar_dados)
 
+editar=uic.loadUi('editar.ui')
     
-
 cadastros.show()
 app.exec()
